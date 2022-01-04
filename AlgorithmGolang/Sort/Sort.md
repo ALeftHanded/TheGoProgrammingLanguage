e782897504510ae84f97b486cd50fa4e0625e172
xiaohan <xiaohan.lu@shopee.com>
2022-01-04 18:45:05 +0800

    feature: add merge sort

---
+++ AlgorithmGolang/Sort/Sort.md
@@ -0,0 +1,12 @@
+### Sort in Golang
+
+> https://www.geeksforgeeks.org/sorting-algorithms/
+
+#### Merge sort
+
+> https://www.geeksforgeeks.org/merge-sort/
+
+Definition
+
+It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves.
+
---
+++ AlgorithmGolang/Sort/mergeSort_test.go
@@ -0,0 +1,82 @@
+package Sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "normal test",
+			args: args{arr: []int{5, 3, 2, 1, 4}},
+			want: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MergeSort(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MergeSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMergeSortedList(t *testing.T) {
+	type args struct {
+		left  []int
+		right []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "normal test",
+			args: args{left: []int{1, 3, 5, 7, 9}, right: []int{2, 4, 6, 8}},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			name: "all number in left smaller than right",
+			args: args{left: []int{1, 2, 3, 4, 5}, right: []int{6, 7, 8, 9}},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			name: "all number in left bigger than right",
+			args: args{right: []int{1, 2, 3, 4, 5}, left: []int{6, 7, 8, 9}},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			name: "left nil",
+			args: args{right: []int{1, 2, 3}},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "right nil",
+			args: args{left: []int{1, 2, 3}},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "all nil",
+			args: args{},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MergeSortedList(tt.args.left, tt.args.right); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MergeSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
