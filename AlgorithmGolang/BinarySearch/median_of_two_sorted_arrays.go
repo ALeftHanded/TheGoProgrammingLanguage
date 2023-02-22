98c2f84ef6efeef02ce061c415d8e06da7d061d3
xiaohan <xiaohan.lu@shopee.com>
2023-02-22 11:33:15 +0800

    feature: add description

--- AlgorithmGolang/BinarySearch/median_of_two_sorted_arrays.go
+++ AlgorithmGolang/BinarySearch/median_of_two_sorted_arrays.go
@@ -1,5 +1,15 @@
 package BinarySearch
 
-func MedianOfTwoSortedArrays() {
+// * 给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
+// * 算法的时间复杂度应该为 O(log (m+n)) 。
 
+// * nums1.length == m
+// * nums2.length == n
+// * 0 <= m <= 1000
+// * 0 <= n <= 1000
+// * 1 <= m + n <= 2000
+// * -10^6 <= nums1[i], nums2[i] <= 10^6
+
+func MedianOfTwoSortedArrays(nums1 []int, nums2 []int) float64 {
+	return -1
 }
