e1cad0ad65057bdfb7198b5a39af00138ad4a388
xiaohan <xiaohan.lu@shopee.com>
2023-04-18 19:04:36 +0800

    feature: add nore string concatenation case

--- GC/inefficient_cases/string_concatenation/string_concatenation.go
+++ GC/inefficient_cases/string_concatenation/string_concatenation.go
@@ -1,6 +1,9 @@
 package string_concatenation
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func ConcatenateStrings(strs []string) string {
 	result := ""
@@ -10,8 +13,29 @@ func ConcatenateStrings(strs []string) string {
 	return result
 }
 
-func ConcatenateStringsOp(strs []string) string {
+func ConcatenateStringsWithBuilder(strs []string) string {
 	var builder strings.Builder
+	for _, str := range strs {
+		builder.WriteString(str)
+	}
+	return builder.String()
+}
+
+func ConcatenateStringsWithJoin(strs []string) string {
+	return strings.Join(strs, "")
+}
+
+func ConcatenateStringsWithSprintf() string {
+	return fmt.Sprintf("%s%s%s%s%s%s%s%s%s%s", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
+}
+
+func ConcatenateStringsWithBuilderAndGrow(strs []string) string {
+	var builder strings.Builder
+	totalLength := 0
+	for _, str := range strs {
+		totalLength += len(str)
+	}
+	builder.Grow(totalLength)
 	for _, str := range strs {
 		builder.WriteString(str)
 	}
--- GC/inefficient_cases/string_concatenation/string_concatenation_test.go
+++ GC/inefficient_cases/string_concatenation/string_concatenation_test.go
@@ -12,14 +12,35 @@ func BenchmarkConcatenateStrings(b *testing.B) {
 	}
 }
 
-func BenchmarkConcatenateStringsOp(b *testing.B) {
+func BenchmarkConcatenateStringsWithBuilder(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		ConcatenateStringsOp(benchmarkStrs)
+		ConcatenateStringsWithBuilder(benchmarkStrs)
+	}
+}
+
+func BenchmarkConcatenateStringsWithJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ConcatenateStringsWithJoin(benchmarkStrs)
+	}
+}
+
+func BenchmarkConcatenateStringsWithSprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ConcatenateStringsWithSprintf()
+	}
+}
+
+func BenchmarkConcatenateStringsWithBuilderAndGrow(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ConcatenateStringsWithBuilderAndGrow(benchmarkStrs)
 	}
 }
 
 // * go test -bench=. -benchmem
 
-//Function						Operations	Time per operation	Allocated memory per operation
-//BenchmarkConcatenateStrings	4172774		291 ns/op			64 B/op, 9 allocs/op
-//BenchmarkConcatenateStringsOp	13136090	86.9 ns/op			24 B/op, 2 allocs/op
+//Function										       Operations	   Time per operation	Allocated memory per operation
+//BenchmarkConcatenateStrings-16                      	 3739914	       289 ns/op	      64 B/op	       9 allocs/op
+//BenchmarkConcatenateStringsWithBuilder-16           	12547957	        85.5 ns/op	      24 B/op	       2 allocs/op
+//BenchmarkConcatenateStringsWithJoin-16              	12004039	        96.5 ns/op	      16 B/op	       1 allocs/op
+//BenchmarkConcatenateStringsWithSprintf-16           	 4429815	       239 ns/op	      16 B/op	       1 allocs/op
+//BenchmarkConcatenateStringsWithBuilderAndGrow-16    	16140784	        62.1 ns/op	      16 B/op	       1 allocs/op
